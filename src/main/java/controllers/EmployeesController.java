01095c53cb2553278a3c056a50f3c03aac7f08ab
Can Toraman <cantoraman@gmail.com>
2018-07-17 16:03:40 +0100

    can show one employee

--- src/main/java/controllers/EmployeesController.java
+++ src/main/java/controllers/EmployeesController.java
@@ -16,33 +16,41 @@ import static spark.Spark.get;
 public class EmployeesController {
 
 
-    public static void main(String[] args) {
+    public EmployeesController(){
+        this.setupEndPoints();
+    }
+
+        public void setupEndPoints() {
+
+            // when you do this first time you gotta click on the red light bulb that comes and select the
+            // option that makes this accept lambdas
+            get("/employees", (req, res) -> {
+
+                Map<String, Object> model = new HashMap();
+                model.put("template", "templates/employees/index.vtl");
+
+                List<Employee> employees = DBHelper.getAll(Employee.class);
+                model.put("employees", employees);
+                return new ModelAndView(model, "templates/layout.vtl");
+
+            }, new VelocityTemplateEngine());
 
 
-        // when you do this first time you gotta click on the red light bulb that comes and select the
-        // option that makes this accept lambdas
-        get("/employees", (req, res) -> {
+            get("/employees/:id", (request, response) -> {
 
-           Map<String, Object> model = new HashMap();
-           model.put("template", "templates/employees/index.vtl");
+                Map<String, Object> model = new HashMap<>();
+                model.put("template", "templates/employees/show.vtl");
+                int employeeId = Integer.parseInt(request.params(":id"));
+                Employee employee = DBHelper.find(employeeId, Employee.class);
+                model.put("employee", employee);
+                return new ModelAndView(model, "templates/layout.vtl");
 
-           List<Employee> employees = DBHelper.getAll(Employee.class);
-           model.put("employees", employees);
-           return new ModelAndView(model, "templates/layout.vtl");
+            }, new VelocityTemplateEngine());
 
-        }, new VelocityTemplateEngine());
-
-
+        }
 
     }
 
 
 
 
-
-
-
-
-
-
-}
